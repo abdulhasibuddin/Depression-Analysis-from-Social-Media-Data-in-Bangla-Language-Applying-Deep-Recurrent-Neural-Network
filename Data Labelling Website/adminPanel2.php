55e96507a9ed7baf6f55a5e7e9f40ed666efcec6
ABDUL HASIB <abdulhasibuddin@gmail.com>
2018-09-07 21:09:31 +0600

    Update adminPanel2.php

--- Data Labelling Website/adminPanel2.php
+++ Data Labelling Website/adminPanel2.php
@@ -1,10 +1,11 @@
 <?php
 	require 'config.php';
 
-	$sql = "SELECT tweets.* 
+	$sql = "SELECT COALESCE(tweets.label_1, 'Unlabeled') AS tweet_label, 
+			COUNT(tweets.tweet_id) AS label_statistics 
 			FROM tweets 
-			WHERE tweets.label_1 IS NOT NULL;
+			GROUP BY tweets.label_1;
 			";
 	$result = $conn->query($sql);
-	$num_labelled_tweets = $result->num_rows;
+	//$num_labelled_tweets = $result->num_rows;
 ?>
